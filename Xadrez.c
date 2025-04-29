801ed06b8bdaf8e4f8d98e2b81589c929b15c631
Luckas-MendeZ <lucasmendesdev47@gmail.com>
2025-04-29 22:46:45 +0000

    version 0.1

--- Xadrez.c
+++ Xadrez.c
@@ -1,1 +1,11 @@
+#include <stdio.h>
 
+int main(){
+    //movimentação torre
+        int casastorre=5 //quantidade de casas que precisa mover
+        int torrelugar=1 //casa atual da torre
+    //movimentação rainha
+        
+    //movimentação bispo
+
+}
