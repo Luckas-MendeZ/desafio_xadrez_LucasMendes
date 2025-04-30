ecd6249d05482da4b8111abcbcba90bb1cf4c4a3
Luckas-MendeZ <lucasmendesdev47@gmail.com>
2025-04-29 23:15:37 -0300

    version 0.2 criei a logica de movimento da torre, rainha e do bispo.

--- Xadrez.c
+++ Xadrez.c
@@ -2,10 +2,54 @@
 
 int main(){
     //movimentação torre
-        int casastorre=5 //quantidade de casas que precisa mover
-        int torrelugar=1 //casa atual da torre
+        int casastorre=5; //quantidade de casas que precisa mover
+        int torrelugar=0; //casa atual da torre
     //movimentação rainha
-        
+        int casasrainha=8; //quantidade de casas que precisa mover
+        int rainhalugar=0; //casa atual da rainha
     //movimentação bispo
+        int casasbispo=5; //quantidade de casas que precisa mover
+        int casasvert=1; //movimento vertical
+        int casashor=2; //movimento horizontal
 
+    printf("\n***Torre***\n");
+
+    while (torrelugar < casastorre)
+    {
+        printf("Direita\n");
+        torrelugar++;
+    }
+
+    printf("\n***Rainha***\n");
+
+    do
+    {
+        printf("Esquerda\n");
+        rainhalugar++;
+    } while (rainhalugar < casasrainha);
+
+    printf("\n***Bispo***\n");
+
+    for (int i = 0; i < casasbispo; i++){
+
+        switch (casasvert) {
+        case 1:
+            printf("Cima, ");
+            break;
+        
+        default:
+            printf("invalido");
+            break;
+        } 
+        switch (casashor) {
+        case 2:
+            printf("Direita\n");
+            break;
+        
+        default:
+            printf("invalido");
+            break;
+        }
+    }
+    return 0;
 }
